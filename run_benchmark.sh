6e658f503425e62436cb327e786e0e02329302b1
Karl Osswald <ansovald@posteo.de>
2025-09-05 12:29:37 +0000

    minor changes

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,26 +2,29 @@
 # Usage: scripts/run_benchmark.sh
 
 # do this if running HuggingFace models so that the model weights are downloaded under this directory. For API-based models, it can be ignored.
-export HUGGINGFACE_HUB_CACHE=/data/hakimov/huggingface_cache
+export HUGGINGFACE_HUB_CACHE=/data/kosswald/huggingface_cache
 
 # set the GPU#0 to be used for inferencing. For API-based models, it can be ignored
-export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=3
 
-# activate  the virtual environment, assuming that the virtual environment files are located under "venv" folder (adjust if it is set up differently)
-source venv/bin/activate
+# activate HF venv:
+source /project/kosswald/paper2025/hf_venv/bin/activate
 export PYTHONPATH=.:$PYTHONPATH
 
 mkdir -p logs
 
 
 games=(
-"taboo"
+"clean_up"
 )
 
 # choose models to run, from global list (clem list models) or local list (the ones in model_registry.json)
 
 models=(
-"gpt-4o-2024-08-06"
+# "Meta-Llama-3.1-8B-Instruct"
+# "DeepSeek-R1-0528-Qwen3-8B"
+"gpt-oss-20b-nothink"
 )
 
 echo
@@ -36,9 +39,9 @@ for game in "${games[@]}"; do
     echo "Testing ${model} on ${game}"
     # currently, instances.json is the default file for the current run
     # to input a specific instances file, so we could also use the version number here like this: -i instances_file (no need to .json extension)
-    { time clem run -g "${game}" -m "${model}" -r "results"; } 2>&1 | tee logs/runtime."${game}"."${model}".log
-    { time clem transcribe -g "${game}" -r "results"; } 2>&1 | tee logs/runtime.transcribe."${game}".log
-    { time clem score -g "${game}" -r "results"; } 2>&1 | tee logs/runtime.score."${game}".log
+    { time clem run -g "${game}" -m "${model}"; } 2>&1 | tee logs/runtime."${game}"."${model}".log
+    { time clem transcribe -g "${game}"; } 2>&1 | tee logs/runtime.transcribe."${game}".log
+    { time clem score -g "${game}"; } 2>&1 | tee logs/runtime.score."${game}".log
   done
 done
 echo "Evaluating results"
